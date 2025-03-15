a30fe88c0ed5ffeddacaeb9e32d5720bcadae2c4
sally An <sally03915@gmail.com>
2025-03-15 22:49:48 +0900

    ch1_next  2025-03-15

---
+++ project/front/ch1_next/pages/index.js
@@ -0,0 +1,7 @@
+import React from  'react';
+import AppLayout from '../components/AppLayout';
+
+const Home = () =>{
+   return (<AppLayout> Hello, Next </AppLayout>);
+}
+export default Home;
---
+++ project/front/ch1_next/pages/signup.js
@@ -0,0 +1,7 @@
+import React from  'react';
+import AppLayout from '../components/AppLayout';
+
+const Signup = () =>{
+   return (<AppLayout> 회원가입페이지 </AppLayout>);
+}
+export default Signup;
